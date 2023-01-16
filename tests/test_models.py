5c78ba88bf61393c493cd2f40a553de80e0cdcdd
kyllker <kymargar@gmail.com>
2023-01-17 00:47:48 +0100

    fix/supervised-and-imputer

--- tests/test_models.py
+++ tests/test_models.py
@@ -35,12 +35,12 @@ class TestSupervisedModel:
         x, y = load_iris(return_X_y=True)
         df_iris = pd.DataFrame(data=np.c_[x, y],
                                columns=self.iris['feature_names'] + ['target'])
-        model = self.supervised_model_object.xgboost_model(df_iris.iloc[:, :-1], y)
+        model = self.supervised_model_object.xgboost_model(df_iris.iloc[:, :-1], df_iris['target'])
         assert isinstance(model, xgb.XGBRegressor)
 
     def test_lightgbm_model(self):
         x, y = load_iris(return_X_y=True)
         df_iris = pd.DataFrame(data=np.c_[x, y],
                                columns=self.iris['feature_names'] + ['target'])
-        model = self.supervised_model_object.lightgbm_model(df_iris.iloc[:, :-1], y)
+        model = self.supervised_model_object.lightgbm_model(df_iris.iloc[:, :-1], df_iris['target'])
         assert isinstance(model, lgb.LGBMRegressor)
